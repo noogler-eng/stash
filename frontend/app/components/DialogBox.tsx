c6cab95d30a94e688e49848ca0f3d7c22f8ad535
noogler-eng <sharadpoddar10102003@gmail.com>
2024-12-05 01:46:34 +0530

    add

--- frontend/app/components/DialogBox.tsx
+++ frontend/app/components/DialogBox.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -9,46 +10,85 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
 import { currentUser } from "@/hooks/user";
+import { useState } from "react";
 
 export function DialogDemo({ children }: { children: React.ReactNode }) {
   const user = currentUser();
+  const [content, setContent] = useState("");
+  const [file, setFile] = useState<File | null>();
+  const [imageUrl, setImageUrl] = useState("");
+
+  const handelPost = async () => {};
+
+  const handelGenerateWithAi = async () => {};
+
+  console.log(imageUrl);
 
   return (
     <Dialog>
       <DialogTrigger asChild>{children}</DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
+        <DialogHeader onSubmit={handelPost}>
+          <DialogTitle>Tweet</DialogTitle>
           <DialogDescription>
-            Make changes to your profile here. Click save when you're done.
+            Make sure this is available to everyone. Click post when you're
+            done.
           </DialogDescription>
         </DialogHeader>
         <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input
+          <div className="flex gap-2 items-center">
+            {user.user?.profileImg && (
+              <img
+                src={user.user?.profileImg}
+                width={50}
+                className="rounded-full"
+              />
+            )}
+            <div className="flex flex-col bg-gray-100 hover:bg-gray-200 justify-center p-2 rounded-xl">
+              <p className="text-sm font-extrabold">{user.user?.name}</p>
+              <p className="text-sm">{user.user?.email}</p>
+            </div>
+          </div>
+          <div className="w-full items-center gap-4">
+            <Textarea
               id="name"
-              defaultValue="Pedro Duarte"
+              placeholder="What's happening?"
               className="col-span-3"
+              rows={10}
             />
           </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Username
-            </Label>
+          <div className="w-full items-center gap-4">
             <Input
-              id="username"
-              defaultValue="@peduarte"
+              type="file"
+              id="image"
               className="col-span-3"
+              onChange={async (e) => {
+                const incommingFile = e?.target?.files?.[0];
+                if (!incommingFile) return;
+                setFile(incommingFile);
+                const arrayBuffer = await incommingFile?.arrayBuffer();
+                const blob = new Blob([arrayBuffer], { type: file?.type });
+                const objectURL = URL.createObjectURL(blob);
+                setImageUrl(objectURL);
+              }}
+              accept="image/*"
             />
+          </div>
+          {imageUrl && (
+            <div className="w-full">
+              <img src={imageUrl} className="w-1/2" />
+            </div>
+          )}
+          <div className="w-full items-center gap-4">
+            <Button id="name" className="w-full" onClick={handelGenerateWithAi}>
+              generate with ai
+            </Button>
           </div>
         </div>
         <DialogFooter>
-          <Button type="submit">Save changes</Button>
+          <Button type="submit">post</Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
--- frontend/app/components/PostBox.tsx
+++ frontend/app/components/PostBox.tsx
@@ -1,5 +1,6 @@
 import { currentUser } from "@/hooks/user";
 import { DialogDemo } from "./DialogBox";
+import { Image } from "lucide-react";
 
 export default function PostBox() {
   const user = currentUser();
@@ -8,20 +9,28 @@ export default function PostBox() {
     <DialogDemo>
       <div>
         {user && (
-          <div className="p-2 flex gap-2 items-center bg-gray-100">
+          <div className="p-2 flex gap-4 items-start bg-gray-100 hover:bg-gray-200 rounded-lg ">
             {user.user?.profileImg && (
               <img
                 src={user.user?.profileImg}
                 className="rounded-full"
-                width={50}
+                width={40}
               />
             )}
-            <input
-              type="text"
-              className="w-full border-b focus:outline-none rounded-lg py-2 p-3"
-              placeholder="what's happening?"
-              onClick={() => {}}
-            />
+            <div className="flex flex-col gap-1 justify-center w-full">
+              <div className="text-gray-500 font-extrabold border border-2 border-gray-500 rounded-xl px-2 w-fit">
+                everyone
+              </div>
+              <input
+                type="text"
+                className="w-full border-b focus:outline-none rounded-lg py-2 bg-transparent w-full"
+                placeholder="what's happening?"
+                onClick={() => {}}
+              />
+              <div>
+                <Image size={18}/>
+              </div>
+            </div>
           </div>
         )}
       </div>
